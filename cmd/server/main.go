7d12281d46fb7aed0c94a5730b513e6fda9d4211
adithya-kalaparthy <adithya.kalaparthy@gmail.com>
2025-06-25 22:52:51 +0200

    feat: modify main.go
    main.go file has been modified to use config and routes to
    load env files and setup routes when the api is started.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,19 +1,34 @@
 package main
 
 import (
+	"log"
+
+	"github.com/adithya-kalaparthy/samvad/internal/api/middlewares"
+	"github.com/adithya-kalaparthy/samvad/internal/api/routes"
+	"github.com/adithya-kalaparthy/samvad/internal/config"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
+	router := gin.Default()
+	err := config.LoadEnv()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Setup routes.
+	routerGroup := router.Group("/api")
+	// Define your middlewares in a slice
+	// The order here matters, as middlewares are executed in the order they are added.
+	var apiMiddlewares = []gin.HandlerFunc{
+		middlewares.AuthorizeMiddleware(),
+		middlewares.RequestIdMiddleware(),
+	}
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-
-	if err := r.Run(); err != nil {
-		panic(err)
+	routerGroup.Use(apiMiddlewares...)
+	routes.SetupRoutes(routerGroup)
+	// Start the server.
+	routerErr := router.Run(":8080")
+	if routerErr != nil {
+		log.Fatalf("Server failed to start: %v", routerErr) // Log the error and exit
 	}
 }
