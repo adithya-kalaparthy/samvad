28205c87d834d1077e1c34a5eed39b9f1bda227c
adithya-kalaparthy <adithya.kalaparthy@gmail.com>
2026-06-29 22:37:45 +0200

    feat: create a script to push docker image to ecr
    A new bash script is added to push the docker image to aws ecr.
    This is just for learning purposes. This wont be used as a part
    of ci/cd.

---
+++ scripts/sandbox/push_to_ecr.sh
@@ -0,0 +1,20 @@
+set -euo pipefail
+
+REGION="eu-north-1"
+ACCOUNT="572391417926"
+REPO_NAME="samvad-api"
+IMAGE_TAG="latest"
+ECR_URI="$ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$REPO_NAME"
+
+echo "---- Authenticating with ECR ----"
+aws ecr get-login-password --region "$REGION" | \
+  docker login --username AWS --password-stdin "$ECR_URI"
+
+echo "---- Building Docker image ----"
+docker build -t "$REPO_NAME:$IMAGE_TAG" ../../.
+
+echo "---- Tagging image with ECR URI ----"
+docker tag "$REPO_NAME:$IMAGE_TAG" "$ECR_URI:$IMAGE_TAG"
+
+echo "---- Pushing to ECR ----"
+docker push "$ECR_URI:$IMAGE_TAG"
